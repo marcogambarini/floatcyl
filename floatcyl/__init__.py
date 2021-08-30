23195d2cd2f00bcf9b4877b71e48f3ce56636db0
Marco Gambarini <marco.gambarini@mail.polimi.it>
2021-08-30 10:10:57 +0200

    Added __init__.py for automatic import of submodules when importing floatcyl

---
+++ floatcyl/__init__.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python3
+
+from floatcyl.disp_relation.disp_relation import *
+from floatcyl.isolated_body.isolated_body import *
+from floatcyl.array_interaction.array_interaction import *
