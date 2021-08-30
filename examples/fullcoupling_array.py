9880ca3506e0362b70b8b434beb245e42eeec5ef
Marco Gambarini <marco.gambarini@mail.polimi.it>
2021-08-30 14:33:45 +0200

    Added first example. The example will be used as a guide on how to build the classes.

---
+++ examples/fullcoupling_array.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+
+
+import floatcyl as fcl
+import numpy as np
+
+omega = 3.
+depth = 30.
+beta = 0.
+rho = 1000.
+g = 9.81
+Nbodies = 4
+
+a = 1.
+spacing = 10.
+draft = 0.5
+
+
+# 1) Dispersion relation
+k = fcl.real_disp_rel(omega, depth)
+
+print('Wavelength = ', 2*np.pi/k, ' m')
+
+Nq = 50 #number of evanescent modes
+kq = fcl.imag_disp_rel(omega, depth, Nq)
+
+Nn = 5 #number of progressive modes
