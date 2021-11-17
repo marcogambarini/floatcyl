d09b131fcce30d9686646b6749d857781a743edf
Marco Gambarini <marco.gambarini@mail.polimi.it>
2021-11-17 16:19:06 +0100

    Corrected example on fullcoupling Linear array centered in the origin, reduced to 5 bodies

--- examples/fullcoupling_array.py
+++ examples/fullcoupling_array.py
@@ -1,15 +1,19 @@
 #! /usr/bin/env python3
 
+"""
+Full coupling (diffraction + radiation) for a linear array
+"""
 
 import floatcyl as fcl
 import numpy as np
+import matplotlib.pyplot as plt
 
 omega = 1.
-depth = 30.
+depth = 10.
 beta = 0.
 rho = 1000.
 g = 9.81
-Nbodies = 100
+Nbodies = 5
 
 a = 1.
 spacing = 10.
@@ -36,7 +40,7 @@ body = fcl.Cylinder(radius=a, draft=draft)
 cylArray = fcl.Array(beta=beta, depth=depth, k=k, kq=kq, Nn=Nn, Nq=Nq,
                      omega=omega, water_density=rho, g=g)
 for ii in range(Nbodies):
-    cylArray.add_body(0., ii*spacing, body)
+    cylArray.add_body(0., ii*spacing-(Nbodies-1)*spacing/2, body)
 
 # 4) compute isolated body behaviour (only once!)
 body.compute_diffraction_properties()
@@ -48,10 +52,17 @@ cylArray.solve()
 print("rao = ", cylArray.rao)
 
 # 6) compute the free surface elevation
-# nx = 100
-# ny = 40
-# Lx = 50.
-# Ly = 20.
-# x = np.linspace(-Lx, Lx, nx)
-# y = np.linspace(-Lx, Lx, ny)
-# eta = cylArray.compute_free_surface(x, y)
+nx = 100
+ny = 40
+Lx = 50.
+Ly = 20.
+x = np.linspace(-Lx, Lx, nx)
+y = np.linspace(-Lx, Lx, ny)
+eta = cylArray.compute_free_surface(x, y)
+
+
+fig, ax = plt.subplots()
+cs = ax.contourf(x, y, np.abs(eta))
+cbar = plt.colorbar(cs)
+
+plt.show()
